b5fbb0dd3ff7ae02cd094ed58ca24909874cd3c2
Steve <steveriven94@gmail.com>
2024-05-08 11:42:19 +0800

    update cv and link

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="/resume/CV-Steve.pdf" target="_blank" rel="noopener noreferrer"
+                  href="/resume/Ka_Ho_Yip.pdf" target="_blank" rel="noopener noreferrer"
                 >
                   Resume
                 </a>
