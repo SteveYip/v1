04d6863bfe5bc4dab70a3ec2f836fd2eaea35ddf
Steve <steveriven94@gmail.com>
2024-07-24 17:48:25 +0800

    update error

--- pages/index.js
+++ pages/index.js
@@ -123,9 +123,9 @@ export default function Home() {
                 
                 <div className="-my-6">
                     <div className="relative pl-8 sm:pl-32 py-6 group">
-                        <div className="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">System Analyst</div>
+                        <div className="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">Senior Full Stack Engineer</div>
                         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50  sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600  ">June, 2022</time>
+                            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600  ">June, 2022 - Present</time>
                             <div className="text-xl font-bold text-slate-900">AIA Hong Kong & Macau</div>
                         </div>
                         <div className="text-slate-500">I excel in enhancing system integrations and upgrades. My role involves mapping, documenting, and ensuring system compatibility. I work closely with developers, testers, and users, utilizing JAVA, Spring, Spring Boot, and MongoDB to troubleshoot and optimize technical functionalities. My focus is on maintainable code and software enhancement.</div>
